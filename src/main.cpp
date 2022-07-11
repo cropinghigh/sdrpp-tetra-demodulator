256eddd10801ca722ba7019b91e47f3640b99cd7
cropinghigh indir <joinmark60@gmail.com>
2022-07-11 17:58:31 +0300

    Update for new dsp + demod input fix

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,19 @@
 #include <gui/gui.h>
 #include <signal_path/signal_path.h>
 #include <module.h>
+#include <unistd.h>
+#include <fstream>
 
-#include <dsp/demodulator.h>
-#include <dsp/processing.h>
-#include <dsp/routing.h>
+#include <dsp/demod/psk.h>
+#include <dsp/buffer/packer.h>
+#include <dsp/routing/splitter.h>
+#include <dsp/stream.h>
+#include <dsp/sink/handler_sink.h>
 
 #include <gui/widgets/constellation_diagram.h>
 #include <gui/widgets/file_select.h>
 
 #include "symbol_extractor.h"
-
-
-#include <unistd.h>
 
 #define CONCAT(a, b)    ((std::string(a) + b).c_str())
 
@@ -25,7 +26,7 @@ SDRPP_MOD_INFO {
     /* Name:            */ "tetra_demodulator",
     /* Description:     */ "Tetra demodulator for SDR++(output can be fed to tetra-rx from osmo-tetra)",
     /* Author:          */ "cropinghigh",
-    /* Version:         */ 0, 0, 2,
+    /* Version:         */ 0, 0, 3,
     /* Max instances    */ -1
 };
 
@@ -35,7 +36,7 @@ class TetraDemodulatorModule : public ModuleManager::Instance {
 public:
     TetraDemodulatorModule(std::string name) {
         this->name = name;
-        
+
         // Load config
         config.acquire();
         bool created = false;
@@ -47,54 +48,42 @@ public:
         strcpy(filePath, std::string(config.conf[name]["filePath"]).c_str());
         fileOpen = config.conf[name]["fileOpen"];
         config.release(created);
-        
+
         vfo = sigpath::vfoManager.createVFO(name, ImGui::WaterfallVFO::REF_CENTER, 0, 29000, 36000, 29000, 29000, true);
-        
-        mainDemodulator.init(vfo->output, 36000, 18000, 31, 0.35, 0.4f, debugCostasBw, (0.01*0.01) / 4, 0.01f, 0.005f);
-        constDiagSplitter.init(mainDemodulator.out);
+        mainDemodulator.init(vfo->output, 18000, 36000, 31, 0.35, 0.4f, debugCostasBw, (0.01*0.01) / 4, 0.01f, 0.01f);
+        constDiagSplitter.init(&mainDemodulator.out);
         constDiagSplitter.bindStream(&constDiagStream);
         constDiagSplitter.bindStream(&demodStream);
-        
         constDiagReshaper.init(&constDiagStream, 1024);
         constDiagSink.init(&constDiagReshaper.out, _constDiagSinkHandler, this);
-        
         diffDecoder.init(&demodStream);
-        
         constDiag2Splitter.init(&diffDecoder.out);
         constDiag2Splitter.bindStream(&constDiag2Stream);
         constDiag2Splitter.bindStream(&demod2Stream);
-        
         constDiag2Reshaper.init(&constDiag2Stream, 1024);
         constDiag2Sink.init(&constDiag2Reshaper.out, _constDiag2SinkHandler, this);
-        
         symbolExtractor.init(&demod2Stream);
-        
         bitsUnpacker.init(&symbolExtractor.out);
         demodSink.init(&bitsUnpacker.out, _demodSinkHandler, this);
-        
+
         if(fileOpen) {
             outfile = std::ofstream(filePath, std::ios::binary);
             if(!outfile.is_open()) {
                 spdlog::error("Error opening file for tetra_demodulator output!");
             }
         }
-        
+
         mainDemodulator.start();
-        
         constDiagSplitter.start();
         constDiagReshaper.start();
         constDiagSink.start();
-        
         diffDecoder.start();
-        
         constDiag2Splitter.start();
         constDiag2Reshaper.start();
         constDiag2Sink.start();
-        
         symbolExtractor.start();
         bitsUnpacker.start();
         demodSink.start();
-        
         gui::menu.registerEntry(name, menuHandler, this, this);
     }
 
@@ -109,22 +98,19 @@ public:
 
     void enable() {
         vfo = sigpath::vfoManager.createVFO(name, ImGui::WaterfallVFO::REF_CENTER, 0, 29000, 36000, 29000, 29000, true);
+        mainDemodulator.setInput(vfo->output);
         mainDemodulator.start();
-        
         constDiagSplitter.start();
         constDiagReshaper.start();
         constDiagSink.start();
-        
         diffDecoder.start();
-        
         constDiag2Splitter.start();
         constDiag2Reshaper.start();
         constDiag2Sink.start();
-        
         symbolExtractor.start();
         bitsUnpacker.start();
         demodSink.start();
-        
+
         if(outfile.is_open()) {
             outfile.close();
         }
@@ -134,24 +120,18 @@ public:
                 spdlog::error("Error opening file for tetra_demodulator output!");
             }
         }
-        
-        demodSink.start();
         enabled = true;
     }
 
     void disable() {
         mainDemodulator.stop();
-        
         constDiagSplitter.stop();
         constDiagReshaper.stop();
         constDiagSink.stop();
-        
         diffDecoder.stop();
-        
         constDiag2Splitter.stop();
         constDiag2Reshaper.stop();
         constDiag2Sink.stop();
-        
         symbolExtractor.stop();
         bitsUnpacker.stop();
         demodSink.stop();
@@ -173,63 +153,68 @@ private:
 
         float menuWidth = ImGui::GetContentRegionAvail().x;
 
-        if (_this->enabled) {
-            if(ImGui::SliderFloat(CONCAT("Costas loop bandwidth##_tetrademod_costasbw_", _this->name), &_this->debugCostasBw, 0.0f, 0.05f, "%.6f")) {
-                _this->mainDemodulator.setCostasLoopBw(_this->debugCostasBw); //NOT BUG, BUT A FEATURE!(i currently don't know how to automatically reduce costas loop bandwidth, when it synchronizes to carrier)
-            }
-            if (_this->fileOpen && _this->enabled) { style::beginDisabled(); }
-            if(ImGui::InputText(CONCAT("Output file path:##_tetrademod_outfile_", _this->name), _this->filePath, 2047)) {
-                config.acquire();
-                config.conf[_this->name]["filePath"] = std::string(_this->filePath);
-                config.release(true);
-            }
-            if (_this->fileOpen && _this->enabled) { style::endDisabled(); }
-            if (ImGui::Checkbox(CONCAT("Write to file##_tetrademod_writefile_", _this->name), &_this->fileOpen)) {
-                if(_this->fileOpen) {
-                    if(_this->outfile.is_open()) {
-                        _this->outfile.close();
-                    }
-                    _this->outfile = std::ofstream(_this->filePath, std::ios::binary);
-                    if(!_this->outfile.is_open()) {
-                        spdlog::error("Error opening file for tetra_demodulator output!");
-                    }
-                } else {
-                    if(_this->outfile.is_open()) {
-                        _this->outfile.close();
-                    }
+        if(!_this->enabled) {
+            style::beginDisabled();
+        }
+
+        if(ImGui::SliderFloat(CONCAT("Costas loop bandwidth##_tetrademod_costasbw_", _this->name), &_this->debugCostasBw, 0.0f, 0.05f, "%.6f") && _this->enabled) {
+            _this->mainDemodulator.setCostasBandwidth(_this->debugCostasBw); //NOT A BUG, BUT A FEATURE!(i don't know how to automatically reduce costas loop bandwidth, when it synchronizes to the carrier)
+        }
+        if (_this->fileOpen && _this->enabled) { style::beginDisabled(); }
+        if(ImGui::InputText(CONCAT("Output file path:##_tetrademod_outfile_", _this->name), _this->filePath, 2047)) {
+            config.acquire();
+            config.conf[_this->name]["filePath"] = std::string(_this->filePath);
+            config.release(true);
+        }
+        if (_this->fileOpen && _this->enabled) { style::endDisabled(); }
+        if (ImGui::Checkbox(CONCAT("Write to file##_tetrademod_writefile_", _this->name), &_this->fileOpen) && _this->enabled) {
+            if(_this->fileOpen) {
+                if(_this->outfile.is_open()) {
+                    _this->outfile.close();
                 }
-                config.conf[_this->name]["fileOpen"] = _this->fileOpen;
+                _this->outfile = std::ofstream(_this->filePath, std::ios::binary);
+                if(!_this->outfile.is_open()) {
+                    spdlog::error("Error opening file for tetra_demodulator output!");
+                }
+            } else {
+                if(_this->outfile.is_open()) {
+                    _this->outfile.close();
+                }
             }
-            ImGui::Text("Demodulated: ");
-            ImGui::SetNextItemWidth(menuWidth);
-            _this->constDiag.draw();
-            
-            ImGui::Text("Diff decoded: ");
-            ImGui::SetNextItemWidth(menuWidth);
-            _this->constDiag2.draw();
+            config.conf[_this->name]["fileOpen"] = _this->fileOpen;
+        }
+        ImGui::Text("Demodulated: ");
+        ImGui::SetNextItemWidth(menuWidth);
+        _this->constDiag.draw();
+
+        ImGui::Text("Diff decoded: ");
+        ImGui::SetNextItemWidth(menuWidth);
+        _this->constDiag2.draw();
+        if(!_this->enabled) {
+            style::endDisabled();
         }
     }
-    
+
     static void _constDiagSinkHandler(dsp::complex_t* data, int count, void* ctx) {
         TetraDemodulatorModule* _this = (TetraDemodulatorModule*)ctx;
-        
+
         dsp::complex_t* cdBuff = _this->constDiag.acquireBuffer();
         if(count == 1024) {
             memcpy(cdBuff, data, count * sizeof(dsp::complex_t));
         }
         _this->constDiag.releaseBuffer();
     }
-    
+
     static void _constDiag2SinkHandler(dsp::complex_t* data, int count, void* ctx) {
         TetraDemodulatorModule* _this = (TetraDemodulatorModule*)ctx;
-        
+
         dsp::complex_t* cdBuff = _this->constDiag2.acquireBuffer();
         if(count == 1024) {
             memcpy(cdBuff, data, count * sizeof(dsp::complex_t));
         }
         _this->constDiag2.releaseBuffer();
     }
-    
+
     static void _demodSinkHandler(uint8_t* data, int count, void* ctx) {
         TetraDemodulatorModule* _this = (TetraDemodulatorModule*)ctx;
         if(_this->outfile.is_open()) {
@@ -239,37 +224,36 @@ private:
 
     std::string name;
     bool enabled = true;
-    
+
     VFOManager::VFO* vfo;
-    
-    dsp::PSKDemod<4, false> mainDemodulator;
-    
-    float debugCostasBw = 0.001f;
-    dsp::Splitter<dsp::complex_t> constDiagSplitter;
+
+    dsp::demod::PSK<4> mainDemodulator;
+
+    float debugCostasBw = 0.005f;
+    dsp::routing::Splitter<dsp::complex_t> constDiagSplitter;
     dsp::stream<dsp::complex_t> constDiagStream;
-    dsp::Packer<dsp::complex_t> constDiagReshaper;
-    dsp::HandlerSink<dsp::complex_t> constDiagSink;
+    dsp::buffer::Packer<dsp::complex_t> constDiagReshaper;
+    dsp::sink::Handler<dsp::complex_t> constDiagSink;
     ImGui::ConstellationDiagram constDiag;
-    
+
     dsp::stream<dsp::complex_t> demodStream;
-    
-    dsp::Splitter<dsp::complex_t> constDiag2Splitter;
+
+    dsp::routing::Splitter<dsp::complex_t> constDiag2Splitter;
     dsp::stream<dsp::complex_t> constDiag2Stream;
-    dsp::Packer<dsp::complex_t> constDiag2Reshaper;
-    dsp::HandlerSink<dsp::complex_t> constDiag2Sink;
+    dsp::buffer::Packer<dsp::complex_t> constDiag2Reshaper;
+    dsp::sink::Handler<dsp::complex_t> constDiag2Sink;
     ImGui::ConstellationDiagram constDiag2;
-    
+
     dsp::stream<dsp::complex_t> demod2Stream;
-    
+
     dsp::DQPSKSymbolExtractor symbolExtractor;
     dsp::DifferentialDecoder diffDecoder;
     dsp::BitUnpacker bitsUnpacker;
-    dsp::HandlerSink<uint8_t> demodSink;
-    
+    dsp::sink::Handler<uint8_t> demodSink;
+
     char filePath[2048];
     bool fileOpen = false;
     std::ofstream outfile;
-    
 
 };
 
--- src/symbol_extractor.h
+++ src/symbol_extractor.h
@@ -1,27 +1,22 @@
 #pragma once
-#include <dsp/block.h>
+#include <dsp/processor.h>
 
 namespace dsp {
-    
     //block that extracts symbols from DQPSK constellation
-    class DQPSKSymbolExtractor : public generic_block<DQPSKSymbolExtractor> {
+    class DQPSKSymbolExtractor : public Processor<complex_t, uint8_t> {
+        using base_type = Processor<complex_t, uint8_t>;
     public:
         DQPSKSymbolExtractor() {}
 
-        void init(stream<complex_t>* in) {
-            _in = in;
-            generic_block<DQPSKSymbolExtractor>::registerInput(_in);
-            generic_block<DQPSKSymbolExtractor>::registerOutput(&out);
-            generic_block<DQPSKSymbolExtractor>::_block_init = true;
-        }
+        DQPSKSymbolExtractor(stream<complex_t>* in) {init(in);}
 
         int run() {
-            count = _in->read();
+            count = base_type::_in->read();
             if(count < 0) { return -1; }
 
             for(int i = 0; i < count; i++) {
 //                 complex_t sym = _in->readBuf[i];
-                complex_t sym_c = _in->readBuf[i];
+                complex_t sym_c = base_type::_in->readBuf[i];
 //                 complex_t z, sym_rot;
 //                 z.re = cosf(-FL_M_PI / 4.0f);
 //                 z.im = sinf(-FL_M_PI / 4.0f);
@@ -50,44 +45,27 @@ namespace dsp {
 //                 } else {
 //                     bits = 2;  // -pi rotation
 //                 }
-                out.writeBuf[i] = bits;
+                base_type::out.writeBuf[i] = bits;
             }
-            _in->flush();
-            if(!out.swap(count)) { return -1; }
+            base_type::_in->flush();
+            if(!base_type::out.swap(count)) { return -1; }
             return count;
         }
 
-        stream<uint8_t> out;
-
     private:
         int count;
-        stream<complex_t>* _in;
-        //stream<float>* _in;
     };
 
     //Perform differential decode(because it's Dqpsk)
-    class DifferentialDecoder : public generic_block<DifferentialDecoder> {
+    class DifferentialDecoder : public Processor<complex_t, complex_t> {
+        using base_type = Processor<complex_t, complex_t>;
     public:
         DifferentialDecoder() {}
 
-        void init(stream<complex_t>* in) {
-            _in = in;
-            generic_block<DifferentialDecoder>::registerInput(_in);
-            generic_block<DifferentialDecoder>::registerOutput(&out);
-            generic_block<DifferentialDecoder>::_block_init = true;
-        }
-
-        void setInput(stream<complex_t>* in) {
-            assert(generic_block<DifferentialDecoder>::_block_init);
-            generic_block<DifferentialDecoder>::tempStop();
-            generic_block<DifferentialDecoder>::unregisterInput(_in);
-            _in = in;
-            generic_block<DifferentialDecoder>::registerInput(_in);
-            generic_block<DifferentialDecoder>::tempStart();
-        }
+        DifferentialDecoder(stream<complex_t>* in) {init(in);}
 
         int run() {
-            count = _in->read();
+            count = base_type::_in->read();
             if(count < 0) { return -1; }
 
             for(int i = 0; i < count; i++) {
@@ -99,21 +77,19 @@ namespace dsp {
                 complex_t x, y;
                 x.re = prev_sample.re;
                 x.im = -(prev_sample.im);
-                y = _in->readBuf[i] * x;
-                prev_sample = _in->readBuf[i];
+                y = base_type::_in->readBuf[i] * x;
+                prev_sample = base_type::_in->readBuf[i];
                 y = y * rotation_coeff; //rotate constellation by -pi/4
                 //float sym = std::atan2(y.im, y.re);
                 //filter = filter * (1.0f - filter_val) + (sym - 0.0f)*filter_val;
                 //out.writeBuf[i] = dsp::complex_t {sym, 0.0f};
-                out.writeBuf[i] = y;
+                base_type::out.writeBuf[i] = y;
             }
 
-            _in->flush();
-            if(!out.swap(count)) { return -1; }
+            base_type::_in->flush();
+            if(!base_type::out.swap(count)) { return -1; }
             return count;
         }
-
-        stream<complex_t> out;
 
     private:
         int count;
@@ -121,55 +97,31 @@ namespace dsp {
          //float filter_val=0.0001;
         complex_t rotation_coeff = complex_t {cosf(-FL_M_PI / 4.0f), sinf(-FL_M_PI / 4.0f)};
         complex_t prev_sample;
-        stream<complex_t>* _in;
     };
 
     //Unpack 2bits/byte to 1, because tetra-rx needs it
-    class BitUnpacker : public generic_block<BitUnpacker> {
+    class BitUnpacker : public Processor<uint8_t, uint8_t> {
+        using base_type = Processor<uint8_t, uint8_t>;
     public:
         BitUnpacker() {}
 
         BitUnpacker(stream<uint8_t>* in) { init(in); }
 
-        void init(stream<uint8_t>* in) {
-            _in = in;
-
-            generic_block<BitUnpacker>::registerInput(_in);
-            generic_block<BitUnpacker>::registerOutput(&out);
-            generic_block<BitUnpacker>::_block_init = true;
-        }
-
-        void setInput(stream<uint8_t>* in) {
-            assert(generic_block<BitUnpacker>::_block_init);
-            std::lock_guard<std::mutex> lck(generic_block<BitUnpacker>::ctrlMtx);
-            generic_block<BitUnpacker>::tempStop();
-            generic_block<BitUnpacker>::unregisterInput(_in);
-            _in = in;
-            generic_block<BitUnpacker>::registerInput(_in);
-            generic_block<BitUnpacker>::tempStart();
-        }
-
         int run() {
-            int count = _in->read();
+            int count = base_type::_in->read();
             if (count < 0) { return -1; }
             for(int i = 0; i < count; i++) {
-                uint8_t inByte = _in->readBuf[i];
+                uint8_t inByte = base_type::_in->readBuf[i];
 
-                out.readBuf[(i * 2) + 1] = inByte & 0b01;
-                out.readBuf[(i * 2)] = (inByte & 0b10) >> 1;
+                base_type::out.readBuf[(i * 2) + 1] = inByte & 0b01;
+                base_type::out.readBuf[(i * 2)] = (inByte & 0b10) >> 1;
             }
-            _in->flush();
-            if(!out.swap(count * 2)) {
+            base_type::_in->flush();
+            if(!base_type::out.swap(count * 2)) {
                 return -1;
             }
             return count;
         }
-
-        stream<uint8_t> out;
-
-    private:
-
-        stream<uint8_t>* _in;
 
     };
     
