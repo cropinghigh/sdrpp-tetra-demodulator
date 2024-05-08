9d655c3e95af2a83a7ff72f8573c6fab4092cba2
cropinghigh indir <joinmark60@gmail.com>
2024-05-08 16:43:01 +0300

    CI debug

--- .github/workflows/sdrpp_module_hack.cmake
+++ .github/workflows/sdrpp_module_hack.cmake
@@ -1,6 +1,6 @@
 # Get needed values depending on if this is in-tree or out-of-tree
 if (NOT SDRPP_CORE_ROOT)
-    set(SDRPP_CORE_ROOT "../sdrpp_lib/core/src")
+    set(SDRPP_CORE_ROOT "../sdrpp_lib/SDRPlusPlus-nightly/core/src")
 endif ()
 if (NOT SDRPP_MODULE_COMPILER_FLAGS)
     # Compiler flags
