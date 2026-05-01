a104cfd63932ceb7a1bca4b92771d2893cc1413c
EMRE ÇELİK <35144629+EMREOYUN@users.noreply.github.com>
2026-05-01 15:57:44 +0300

    Some fixes
    Minor fixes inside the download_and_patch.sh has been fixed

--- src/decoder/etsi_codec-patches/download_and_patch.sh
+++ src/decoder/etsi_codec-patches/download_and_patch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-URL=http://www.etsi.org/deliver/etsi_en/300300_300399/30039502/01.03.01_60/en_30039502v010301p0.zip
+URL=https://www.etsi.org/deliver/etsi_en/300300_300399/30039502/01.03.01_60/en_30039502v010301p0.zip
 MD5_EXP=a8115fe68ef8f8cc466f4192572a1e3e
 LOCAL_FILE=etsi_tetra_codec.zip
 
@@ -24,7 +24,7 @@ MD5=`md5sum $LOCAL_FILE | awk '{ print $1 }'`
 
 echo Checking MD5SUM ...
 if [ $MD5 != $MD5_EXP ]; then
-	print "MD5sum of ETSI reference codec file doesn't match"
+	echo "MD5sum of ETSI reference codec file doesn't match"
 	exit 1
 fi
 
@@ -34,12 +34,24 @@ unzip -L $PATCHDIR/etsi_tetra_codec.zip
 echo Contents of $CODECDIR:
 ls -lah
 
+echo Fixing filenames...
+cd "$CODECDIR/c-code"
+
+for f in *; do
+    new="$(echo "$f" | tr 'I' 'i')"
+    if [ "$f" != "$new" ]; then
+        mv -i -- "$f" "$new"
+    fi
+done
+
+cd $CODECDIR
+
 echo Applying Patches ...
 ls -lah $PATCHDIR
 cat $PATCHDIR/series
 for p in `cat "$PATCHDIR/series"`; do
 	echo "=> Applying patch '$PATCHDIR/$p'..."
-	patch -p1 < "$PATCHDIR/$p"
+	patch -p1 -d "$CODECDIR" < "$PATCHDIR/$p"
 done
 
 echo Done!
